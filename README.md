772d39bc1a4740831e237670cbcc5bbb6998ca75
caldavidlee <caldavidlee@gmail.com>
2025-09-28 11:59:47 -0700

    Testing OCR with sample photo of Chat log

---
+++ README.md
@@ -0,0 +1,4 @@
+Step 1:
+Create your Virtual Environment
+Step 2:
+Install a bunch of files/dependencies
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import pytesseract
+import argparse
+import cv2
+import os
+
+# Set up the argument parser
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to input image")
+ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to apply to the image")
+args = vars(ap.parse_args())
+
 def main():
-    print("Hello from valorant-sentiment-chat!")
+    #Reads the image from the command line argument
+    image = cv2.imread(args["image"])
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #use Tesseract to OCR the image
+    text = pytesseract.image_to_string(image)
+    print(text)
 
 
 if __name__ == "__main__":
